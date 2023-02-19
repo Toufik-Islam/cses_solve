465880b7579b8b272dc4428a677d2a86ea968cc9
Toufik Islam <112005587+Toufik-Islam@users.noreply.github.com>
2023-02-19 13:54:42 +0600

    Concert tickets.. 19 feb 2023

--- Concert Tickets.cpp
+++ Concert Tickets.cpp
@@ -1,67 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int searching(vector<long long int> A,int fn)
-{
-    if(A.size()==0)
-        return -1;
-    if(A.size()==1)
-        return 0;
-    int l,h,mid=1;
-    l=0;
-    h=A.size()-1;
-
-    while(l<=h)
-    {
-        mid=(l+h)/2;
-        if(A[mid]==fn)
-            return mid;
-        else if(fn<=A[mid])
-            h=mid-1;
-        else
-            l=mid+1;
-    }
-    return mid-1;
-}
-
-
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    long long int m,n;
-    cin>>m>>n;
+    cin.tie(0);
 
-    vector<long long int> A;
-    vector<long long int> B;
-    A.resize(m);
-    B.resize(n);
+    int m,n;
+    cin>>n>>m;
 
-    for(int i=0;i<m;i++)
-        cin>>A[i];
+    multiset<int> A;
 
-    for(int i=0;i<n;i++)
-        cin>>B[i];
-
-    sort(A.begin(),A.end());
-
-//    for(int i=0;i<m;i++)
-//        cout<< A[i]<< " ";
-
-    int chk;
-    for(int i=0;i<n;i++)
+    for(int i=0; i<n; i++)
     {
-        chk = searching(A,B[i]);
-        if(chk==-1)
-            cout<< "-1\n";
-        else if(A[chk]>B[i])
-            cout<< "-1\n";
+        int q;
+        cin >> q;
+        A.insert(q);
+    }
+
+    while(m--)
+    {
+        int x;
+        cin >> x;
+        auto it = A.upper_bound(x);
+        if(A.empty())
+            cout << "-1\n";
+        else if(it==A.begin())
+            cout << "-1\n";
         else
         {
-            cout<< A[chk] << "\n";
-            A.erase(A.begin()+chk);
+            --it;
+            cout << *it << '\n';
+            A.erase(it);
         }
     }
     return 0;
 }
-
